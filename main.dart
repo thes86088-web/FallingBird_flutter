d9627967941479cb7e8b3af0824aaf672c116341
durvesh <thes86088@gmail.com>
2026-09-05 10:40:05 +0530

    add the code for countdown example

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -49,6 +50,9 @@ class ConfiguredCanvas extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Container(
+      
+      child : CountdownWidget(  ),
+      
       height: canvasData.height,
       width: canvasData.width,
       decoration: BoxDecoration(
@@ -63,3 +67,58 @@ class ConfiguredCanvas extends StatelessWidget {
     );
   }
 }
+
+
+
+class CountdownWidget extends StatefulWidget {
+  const CountdownWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CountdownWidget> createState() => _CountdownWidgetState();
+}
+
+class _CountdownWidgetState extends State<CountdownWidget> {
+  Timer? _timer;
+  int _secondsLeft = 10;
+
+  void _startTimer() {
+    // Cancel any existing timer before starting a new one
+    _timer?.cancel(); 
+    
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_secondsLeft > 0) {
+          _secondsLeft--;
+        } else {
+          // Stop the timer when it reaches 0
+          _timer?.cancel(); 
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // ALWAYS cancel the timer when the widget is destroyed to prevent memory leaks
+    _timer?.cancel(); 
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          _secondsLeft > 0 ? '$_secondsLeft' : 'Done!',
+          style: const TextStyle(fontSize: 48),
+        ),
+        ElevatedButton(
+          onPressed: _startTimer,
+          child: const Text('Start Countdown'),
+        ),
+      ],
+    );
+  }
+}
+
